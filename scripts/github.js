c03b410f907fd4f4976eef071e67bb3b13769d57
Levi Bostian <levi@levi.earth>
2023-05-13 06:57:17 -0500

    fix: improve github notifications to only remove dot and not whole button

--- scripts/github.js
+++ scripts/github.js
@@ -1,5 +1,11 @@
-// Hide notification dot as it's a distraction making you think, "I should check my notifications". 
-// Ideally, just the notification dot would hide but I couldn't figure out how to do that so I just hide the entire notification button. 
-Array.from(document.getElementsByTagName("notification-indicator")).forEach(element => {
-  element.style.display = 'none';
+// there is a little dot over the notifications button when you have any unread notifications. I want that dot to not show up. 
+// The dot shows up when a certain class is attached to the button. We observe the button and make sure it never has that class attached
+new MutationObserver(mutations => {
+  mutations.forEach(mutation => {
+    if (mutation.target.classList.contains("AppHeader-button--hasIndicator")) {
+      mutation.target.classList.remove("AppHeader-button--hasIndicator")
+    }
+  })
+}).observe(document.querySelector("#AppHeader-notifications-button"), { 
+  attributes: true 
 });
